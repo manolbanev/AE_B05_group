e4e682835427ba414ea2dc681c55f316d2698098
Manol Banev <manolbanev77@gmail.com>
2023-09-26 16:50:42 +0200

    more polars and graphs added

--- run_xfoil.py
+++ run_xfoil.py
@@ -1,19 +1,20 @@
-"""Runs an XFOIL analysis for a given airfoil and flow conditions"""
 import os
 import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# airfoil_name = "NASA_sc_20710"
-names_of_airfoils = ['NACA2415','NACA_22112','NASA_sc_20610','NASA_sc_20710','NACA_23012','BACXXX','NASA_sc_20410','boing_106','boing_737_midspan']
+names_of_airfoils = ['NACA2415','NACA_22112','NASA_sc_20710','NACA_23012','BACXXX','boeing_106','boeing_737_midspan','NASA_sc_20610','NASA_sc_20410','NASA_sc_20412','NASA_sc_20414']
 file_name = "polar_file.txt"
-alpha_i = 0
+alpha_i = -3
 alpha_f = 3
 alpha_step = 0.1
 Re = 29610844.47
 n_iter = 100
 M = 0.67
+cl_needed = 0.656
+run_simulation = False
+cd_alpha = False
 
 def run_xfoil(airfoil_name):
     if os.path.exists("polar_file_{0}.txt".format(airfoil_name)):
@@ -36,27 +37,60 @@ def run_xfoil(airfoil_name):
     input_file.close()
 
     subprocess.call("xfoil.exe < input_file.in",shell=True)
+if run_simulation:
+    for i in range(11):
+        run_xfoil(names_of_airfoils[i])
+else:
+    figure, axis = plt.subplots(1, 2, figsize=(10,7))
+    figure.tight_layout(pad=2.0)
+    for i in range(1,4):
+        data = np.loadtxt("polar_file_{0}.txt".format(names_of_airfoils[-i]), skiprows=12)
+        cd=[]
+        cl=[]
+        alpha = []
+        for d in data:
+            cd.append(d[2])
+            cl.append(d[1])
+            alpha.append(d[0])
+        axis[0].plot(alpha,cl,label = names_of_airfoils[-i]+" slope: "+  str(np.format_float_positional(np.polyfit(alpha,cl,1)[0],2)),linewidth=3)
+        axis[1].plot(cd,cl,'o',label = names_of_airfoils[-i],linewidth=3)
+    axis[0].set_xlabel(u'\u0251'+u'\u00B0',fontsize=16)
+    axis[0].set_ylabel('Cl',fontsize = 16)
+    axis[0].legend(fontsize = 14, loc="lower right")
+    axis[0].axhline(y = cl_needed,color = 'red')
+    axis[0].grid()
+    axis[1].set_xlabel('Cd',fontsize=16)
+    axis[1].set_ylabel('Cl',fontsize = 16)
+    axis[1].legend(fontsize = 14, loc="lower right")
+    axis[1].grid()
+    plt.show()
 
-    # print(polar_data)
-run_xfoil(names_of_airfoils[-2])
+    # cd-alpha and cl/cd - alpha
+    if cd_alpha:
+        figure, axis = plt.subplots(1, 2, figsize=(10,7))
+        figure.tight_layout(pad=2.0)
+        for i in range(1,4):
+            data = np.loadtxt("polar_file_{0}.txt".format(names_of_airfoils[-i]), skiprows=12)
+            cd=[]
+            cl=[]
+            cl_cd = []
+            alpha = []
+            for d in data:
+                cd.append(d[2])
+                cl.append(d[1])
+                cl_cd.append(d[1]/d[2])
+                alpha.append(d[0])
+            axis[0].plot(alpha,cd,'o',label = names_of_airfoils[-i],linewidth=3)
+            axis[1].plot(alpha,cl_cd,'o',label = names_of_airfoils[-i],linewidth=3)
+        axis[0].set_xlabel(u'\u0251'+u'\u00B0',fontsize=16)
+        axis[0].set_ylabel('Cd',fontsize = 16)
+        axis[0].legend(fontsize = 14, loc="upper left")
+        axis[0].grid()
+        axis[1].set_xlabel(u'\u0251'+u'\u00B0',fontsize=16)
+        axis[1].set_ylabel('Cl/Cd',fontsize = 16)
+        axis[1].legend(fontsize = 14, loc="lower right")
+        axis[1].grid()
+        plt.show()
 
-# figure, axis = plt.subplots(3, 2, figsize=(10,7))
-# figure.tight_layout(pad=2.0)
-# m=0
-# for i in range(3):
-#     for n in range(2):
-#         data = polar_data = np.loadtxt("polar_file_{0}.txt".format(names_of_airfoils[m]), skiprows=12)
-#         cd=[]
-#         cl=[]
-#         alpha = []
-#         for d in data:
-#             cd.append(d[2])
-#             cl.append(d[1])
-#             alpha.append(d[0])
-#         axis[0,0].plot(alpha, cl,'o')
-#         axis[0,0].set_xlabel('Alpha')
-#         axis[0,0].set_ylabel('Cl')
-#         axis[0,0].set_title('Cl/Alpha')
-#         m=m+1
-# plt.show()
+
 
