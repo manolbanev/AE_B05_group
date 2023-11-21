adcc2bf96ba36081e1b702e2497ac51cedd8d9f7
Eliot <eliotazoulay04@gmail.com>
2023-11-21 16:44:43 +0100

    change stiffness file

--- Stiffness.py
+++ Stiffness.py
@@ -1,20 +1,24 @@
 import scipy as sp
-from scipy import interpolate
 import matplotlib.pyplot as plt
+import math
 
+#given
+spar_height = 0.0942 #m
+width = 0.5 #m 
+c_root = 10.10 #m
+c_tip = 2.73 #m
+span = 44.89 #m
+E = 68.9 * 10**9 #m
+G = 26 * 10**9 #m
+#Variables
 N_stringers_top = [4]
 N_stringers_bottom = [4]
 Stringer_change = [0]
 S_stringers = 0.5 #m^2
-
-spar_height = 0.0942 #m
-width = 0.5 #m 
 t = 0.001 #m
-c_root = 10.10
-c_tip = 2.73
-span = 44.89
 
-#chord
+
+#chord and distance
 def chord(a) :
     c = c_root - (c_root - c_tip) * 2 * a / span
     return(c)
@@ -37,37 +41,68 @@ def d(n) :
 nstringertop = sp.interpolate.interp1d(Stringer_change,N_stringers_top,kind="previous",fill_value="extrapolate")
 nstringerbot = sp.interpolate.interp1d(Stringer_change,N_stringers_bottom,kind="previous",fill_value="extrapolate")
 
-#Ixx
+#Stiffness
 def I_xx(y) :  
     I = S_stringers * (width * spar_height * chord(y))**2 * (nstringertop(y) + nstringerbot(y)) + 1/3 * width * chord(y) * (spar_height * chord(y))**2 * t
     return(I)
 
 def J_z(y) :
-    J = 1/3 * spar_height * chord(y) * width * chord(y) * t * (spar_height * chord(y) + width * chord(y)) + S_stringers *chord(y) * (d(nstringertop(y)) + d(nstringerbot(y)))
+    J = 1/3 * spar_height * chord(y) * width * chord(y) * t * (spar_height * chord(y) + width * chord(y)) + S_stringers * ((chord(y))**2 * (d(nstringertop(y))**2 + d(nstringerbot(y))**2))
     return(J)
 
+#Intigrals 
+def v(y) :
+    second = - 1/(I_xx(y) * E)
+    return(second)
+def intigral(y) :
+    intigral, error0 = sp.integrate.quad(v, 0, y)
+    return(intigral)
 
+
+def theta(y) :
+    tha = 1/(G * J_z(y))
+    return(tha)
+
+
+#plotting graph
 x = [0]
 I = [I_xx(0)]
 J = [J_z(0)]
 j = 0
+Deflection = [0]
+Angle = [0]
 while x[-1] < span/2 :
     j = j + 0.01
     x.append(j)
     I.append(I_xx(x[-1]))
     J.append(J_z(x[-1]))
+    estimate1, error1 = sp.integrate.quad(intigral, 0, j)
+    estimate2, error2 = sp.integrate.quad(theta, 0, j)
+    Deflection.append(estimate1)
+    Angle.append(estimate2 * 180/math.pi)
 
-plt.subplot(121)
+
+
+plt.subplot(221)
 plt.plot(x, I)
 plt.title("Moment of inertia diagram")
-plt.xlabel("Spanwise position")
-plt.ylabel("Moment of inertia")
+plt.ylabel("Moment of inertia [m^4]")
 
-plt.subplot(122)
+plt.subplot(223)
 plt.plot(x, J)
 plt.title("Torsional stiffness")
-plt.xlabel("Spanwise position")
-plt.ylabel("Polar moment of inertia")
-print(d(2))
-plt.show()
+plt.xlabel("Spanwise position [m]")
+plt.ylabel("Polar moment of inertia [m^4]")
 
+plt.subplot(222)
+plt.plot(x, Deflection)
+plt.title("Deflection diagram")
+plt.ylabel("Deflection[m]")
+
+plt.subplot(224)    
+plt.plot(x, Angle)
+plt.title("Angle Diagram")
+plt.xlabel("Spanwise position [m]")
+plt.ylabel("Angle [deg]")
+
+plt.show()
