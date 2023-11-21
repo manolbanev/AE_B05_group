3b9b7a3b16b268ef2745a8076743b79a088b3b9d
ivy2137 <marysiabeisert@gmail.com>
2023-11-21 16:36:13 +0100

    finished initial plotting tool

--- plots beun.py
+++ plots beun.py
@@ -1,15 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-Y_n = load(y)
-Y_v =
-Y_m =
 
-def load(pos):
-    return
+def shear(pos):
+    value_shear = pos * 2
+    return value_shear
 
 
+def moment(pos):
+    value_moment = pos + 1
+    return value_moment
 
-plt.plot()
-plt.plot()
+
+def torque(pos):
+    value_torque = pos
+    return value_torque
+
+
+x = np.linspace(0, 23)
+y_shear = shear(x)
+y_moment = moment(x)
+y_torque = torque(x)
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+plt.subplots_adjust(hspace=0.8)
+ax1.plot(x, y_shear, color='red')
+ax1.set_title('Shear')
+ax2.plot(x, y_moment, color='lime')
+ax2.set_title('Moment')
+ax3.plot(x, y_torque, color='blue')
+ax3.set_title('Torque')
 plt.show()
--- propulsion system load.py
+++ propulsion system load.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy as sp
 from scipy import interpolate
 
-# forces undder
+# forces
 forces = np.array([191270, 0, -32480.91])
 
 position = np.array([-1.45, 8.53, 1.7])
