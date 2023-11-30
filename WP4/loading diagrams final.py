74063a0696b836eadd3dde24d3aaf54957ae9f06
ivy2137 <marysiabeisert@gmail.com>
2023-11-30 14:26:44 +0100

    brought back old version

--- WP4/loading diagrams final.py
+++ WP4/loading diagrams final.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
@@ -16,50 +18,54 @@ def get_aerodynamic(x):
     q = 61.25
     c_y = [9.84, 2.45]
     y = [0, 22.445]
-    cl_0 = 0.149649
-    cl_10 = 0.907325
+    cL_0 = 0.149649
+    cL_10 = 0.907325
+
     c_y_func = sp.interpolate.interp1d(y, c_y, kind='linear', fill_value="extrapolate")
+
     file_names = ['MainWing_a=0.00_v=10.00ms.txt', 'MainWing_a=10.00_v=10.00ms.txt']
 
     def load_file(filename):
         data = []  # [y-span,chord,ai,cl,icd,cm]
         used_cols = [0, 1, 2, 3, 5, 7]
         positive_indeces = []
-
         for col in range(len(used_cols)):
             data.append(np.genfromtxt(fname=filename, skip_header=21, skip_footer=1029, usecols=used_cols[col]))
-
-        cl_list = []
-        y_list = [x for x in data[0] if x >= 0]
-
-        for m in range(len(y_list)):
+        Cllst = []
+        Cdlst = []
+        Cmlst = []
+        ylst = [x for x in data[0] if x >= 0]
+        for m in range(len(ylst)):
             for i in range(len(data[0])):
-                if y_list[m] == data[0][i]:
+                if ylst[m] == data[0][i]:
                     positive_indeces.append(i)
+        for n in range(len(ylst)):
+            Cllst.append(data[3][positive_indeces[n]])
+            Cdlst.append(data[4][positive_indeces[n]])
+            Cmlst.append(data[5][positive_indeces[n]])
+        return Cllst, Cdlst, Cmlst, ylst
 
-        for n in range(len(y_list)):
-            cl_list.append(data[3][positive_indeces[n]])
-        return cl_list, y_list
+    def interpolate(Cllst, Cdlst, Cmlst, ylst):
+        Cl_func = sp.interpolate.interp1d(ylst, Cllst, kind='cubic', fill_value='extrapolate')
+        Cd_func = sp.interpolate.interp1d(ylst, Cdlst, kind='cubic', fill_value='extrapolate')
+        Cm_func = sp.interpolate.interp1d(ylst, Cmlst, kind='cubic', fill_value='extrapolate')
+        return Cl_func, Cd_func, Cm_func
 
-    def interpolate(cl_list, y_list):
-        cl_func = sp.interpolate.interp1d(y_list, cl_list, kind='cubic', fill_value='extrapolate')
-        return cl_func
-
-    def L_prime_func(y, cl_func):
-        return cl_func * q * c_y_func(y)
+    def L_prime_func(y, Cl_func):
+        return Cl_func * q * c_y_func(y)
 
     def find_cl_d(alpha):
         coef = math.sin(math.radians(alpha)) / math.sin(math.radians(10))
-        cl_d = coef * (cl_10 - cl_0) + cl_0
-        return cl_d
+        Cl_d = coef * (cL_10 - cL_0) + cL_0
+        return Cl_d
 
     def find_Cl_alpha(y, alpha):
-        cl_d = find_cl_d(alpha)
-        cl_func_0 = interpolate(load_file(file_names[0])[0], load_file(file_names[0])[1], load_file(file_names[0])[2],
+        Cl_d = find_cl_d(alpha)
+        Cl_func_0 = interpolate(load_file(file_names[0])[0], load_file(file_names[0])[1], load_file(file_names[0])[2],
                                 load_file(file_names[0])[3])[0]
-        cl_func_10 = interpolate(load_file(file_names[1])[0], load_file(file_names[1])[1], load_file(file_names[1])[2],
+        Cl_func_10 = interpolate(load_file(file_names[1])[0], load_file(file_names[1])[1], load_file(file_names[1])[2],
                                  load_file(file_names[1])[3])[0]
-        return cl_func_0(y) + ((cl_d - cl_0) / (cl_10 - cl_0)) * (cl_func_10(y) - cl_func_0(y))
+        return Cl_func_0(y) + ((Cl_d - cL_0) / (cL_10 - cL_0)) * (Cl_func_10(y) - Cl_func_0(y))
 
     aerodynamic_output = L_prime_func(x, find_Cl_alpha(x, 0))
 
@@ -67,13 +73,13 @@ def get_aerodynamic(x):
 
 
 def get_inertial(point):
-
-    # constants
-    c_root = 10.10
-    c_tip = 2.73
+    c_root = 10.10  # m
+    c_tip = 2.73  # m
 
     def chord_distribution(x, c_root, c_tip, wingspan):
         return c_root - ((c_root - c_tip) / wingspan) * x
+
+    # plt.plot(span, chord_distribution(span, c_root, c_tip, 22.445))
 
     def A(x):
         chord_at_x = chord_distribution(x, c_root, c_tip, wingspan=22.445)
@@ -127,16 +133,15 @@ def get_integrated_idiot():
     new_values_shear = [i * -1 + 332803.674 for i in values_shear]
     shear_function = sp.interpolate.interp1d(span, new_values_shear, kind='quadratic', fill_value='extrapolate')
 
-    # 0 - 16
     for i in span[:16]:
         values_moment.append(get_moment(shear_function, i))
-    # 16 - 18
+        # 16 - 18
     for i in span[16:18]:
         values_moment.append(get_moment(shear_function, i))
-    # 18 - 40
+        # 18 - 40
     for i in span[18:40]:
         values_moment.append(get_moment(shear_function, i))
-    # 40 - 50
+        # 40 - 50
     for i in span[40:50]:
         values_moment.append(get_moment(shear_function, i))
 
@@ -172,8 +177,3 @@ ax2.set_title('Shear')
 ax3.plot(x, get_integrated_idiot()[1], color='blue')
 ax3.set_title('Moment')
 plt.show()
-
-
-
-
-
