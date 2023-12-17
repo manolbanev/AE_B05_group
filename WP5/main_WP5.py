9b9ac38f215d96c684c5bc11ec80cf761386bbd4
ivy2137 <marysiabeisert@gmail.com>
2023-12-17 18:18:39 +0100

    idk even

--- WP5/main_WP5.py
+++ WP5/main_WP5.py
@@ -1,11 +1,13 @@
 from WP4.USE_THIS_loads_main import get_integrated
 from WP5.wing import wing1
-from WP5.wingbox import Wingbox
+from WP5.wingbox import wingbox1
+from WP5.ribs import rib1
+import matplotlib.pyplot as plt
 
-wingbox1 = Wingbox(0.003)
+sigma_yield = 240 * 1e6
 
 
-def get_shear():
+def get_shear(): # change calculation of the stress in spars using appendix f or sth
     return get_integrated()[0]
 
 
@@ -24,5 +26,65 @@ def get_sigma():
     return sigma_list
 
 
-print(max(get_shear()))
-print(get_sigma())
+def get_stringer_buckling():
+    stringer_buckling = []
+    for i in wing1.span:
+        stringer_buckling.append(wingbox1.stringer.get_buckling(rib1.get_distance(i)))
+    return stringer_buckling
+
+
+def get_web_buckling():
+    web_buckling = []
+    for i in wing1.span:
+        web_buckling.append(wingbox1.spar_rear.get_buckling(i))
+    return web_buckling
+
+
+def check_stringer_failure():
+    sigma = []
+    for i in get_sigma():
+        sigma.append(abs(i))
+    stringer_buckling = get_stringer_buckling()
+    for i in range(len(wing1.span)):
+        if sigma[i] > stringer_buckling[i]:
+            print('Stringer buckling at spanwise location: ', wing1.span[i])
+    print('Done checking stringer failure')
+
+
+def check_spar_failure():
+    tau = get_shear()
+    web_buckling = get_web_buckling()
+    for i in range(len(wing1.span)):
+        if tau[i] > web_buckling[i]:
+            print('Web buckling at spanwise location: ', wing1.span[i])
+    print('Done checking web failure')
+
+
+def check_compressive_failure():
+    sigma = []
+    for i in get_sigma():
+        sigma.append(abs(i))
+    for i in range(len(wing1.span)):
+        if sigma[i] > sigma_yield:
+            print('Compressive failure at spanwise location: ', wing1.span[i])
+    print('Done checking compressive failure')
+
+
+check_stringer_failure()
+check_spar_failure()
+check_compressive_failure()
+
+
+figs, axs = plt.subplots(3)
+plt.tight_layout()
+axs[0].plot(wing1.span, get_sigma(), color='magenta')
+axs[0].set_title('Sigma')
+axs[1].plot(wing1.span, get_stringer_buckling(), color='cyan')
+axs[1].set_title('Stringer Buckling')
+axs[2].plot(wing1.span, get_web_buckling(), color='lime')
+axs[2].set_title('Web Buckling')
+plt.show()
+
+
+
+# implement (in)sanity checks here -> a/b ratio, size of the stringers
